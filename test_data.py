acfed8a20fe533e7176a9c2554dd9960e10a733a
Sharla Passariello <Sharla.Passariello@wales.nhs.uk>
2025-06-27 12:19:29 +0100

    Track test_data.py

---
+++ test_data.py
@@ -0,0 +1,38 @@
+#Test Data used within the functionality of logging in and generating a Radiology ETR
+
+#LOGIN DATA
+TEST_USERNAME = "username" #Replace with Nadex Username before execution
+TEST_PASSWORD = "password" #Replace with Nadex Password before execution
+
+#TEST PATIENT DATA
+TEST_NHS_NUMBER = "nhs number" #Test Patient: Add in NHS number of test patient
+TEST_FEMALE_NHS_NUMBER = "female nhs number" #Test Patient: Add NHS number for female patient - used for max data test
+
+#SIGN OFF FORM DATA
+SIGNOFF_NAME = "Sharla Passariello"
+SIGNOFF_ROLE = "Test Team Principal"
+SIGNOFF_GMC = "GMC123456"
+SIGNOFF_CONTACT_INFO = "07123465789"
+
+#TEST FORM DATA (STANDARD FORM)
+TEST_EXAM = "XR Skull"
+TEST_CLINICAL_QUESTION_STANDARD = "This is an electronic request that has been generated automatically using Selenium with Python."
+SPECIALITY = "Radiology"
+SRC_NAME = "SANDFORD" #Find a test clinician
+PATIENT_LOCATION = "GGH Radiography"
+
+#TEST FORM DATA (MINIMUM FORM)
+TEST_CLINICAL_QUESTION_MINIMUM = "XR"
+
+#TEST FORM DATA (SPECIAL CHARACTERS FORM)
+TEST_CLINICAL_QUESTION_SPECIAL_CHAR =""
+
+
+#TEST FORM DATA (MAXIMUM FORM)
+TEST_CLINICAL_QUESTION_MAXIMUM = ""
+REASON_FOR_RBD =""
+INFECTION_CONTROL_ISSUES =""
+OTHER_REL_DETAILS =""
+RECENT_REL_RESULTS_NOT_LISTED =""
+MRI_OTHER_REL_ADD_DETAILS = ""
+
